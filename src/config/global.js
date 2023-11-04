6567419d8dfdeb1c2b7db4c0f862bba12043a025
flizcanor <flizcanor@icloud.com>
2023-11-03 20:47:19 -0500

    se carga pdf accesible

--- src/config/global.js
+++ src/config/global.js
@@ -161,7 +161,7 @@ export default {
       {
         icono: 'fas fa-file-pdf',
         titulo: 'Descargar PDF',
-        download: 'downloads/prueba.pdf',
+        download: 'downloads/21710115_CF01_DU.zip',
       },
       {
         icono: 'fas fa-download',
